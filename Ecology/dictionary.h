7860aa1875c67e7449a3543edcc3c6dddadc4d9d
afhenry <alex@eduroam-235-34.ucsc.edu>
2015-02-09 13:28:01 -0800

    Initial Dictionary Commit

---
+++ Ecology/dictionary.h
@@ -0,0 +1,17 @@
+//
+//  dictionary.h
+//  Ecology
+//
+//  Created by afhenry on 2/8/15.
+//  Copyright (c) 2015 Chips&Tea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSMutableDictionary* sharedData();
+
+NSMutableDictionary* bData();
+
+NSMutableDictionary* cData();
+
+
